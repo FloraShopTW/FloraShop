65d15948f7cd31714ce796c2daf8878af2bed1c3
BurceaAdina <73740440+BurceaAdina@users.noreply.github.com>
2021-12-06 15:38:33 +0200

    Add files via upload

---
+++ 06.12.2021/UserHome.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Economica|Old+Standard+TT|Playfair+Display+SC" rel="stylesheet">
+        <link rel="stylesheet" href="css-Home.css">
+        <link rel="stylesheet" href="nav.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Petemoss&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body><!--
+        <nav>
+          <ul>
+            <li><a href='#'>HOME </a></li>
+            <li id='shop'><a href='#'>SHOP </a></li>
+            <li><a href='#'>WHERE TO BUY</a></li>
+            <li><a href='#'>ABOUT US</a></li>
+          </ul>
+        </nav>-->
+        <div class="topnav" id="myTopnav">
+          <a href="#"><img src="rose3.png" alt="" ></a>
+          <a href="#home"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+          <a href="./FloraShop.php"><i class="fa fa-shopping-basket" aria-hidden="true"></i> Shop</a>
+          <a href="#contact"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a>
+          <a href="#about"><i class="fa fa-question-circle-o" aria-hidden="true"></i> About</a>
+          <a href="./Logout.php"><i class="fa fa-fw fa-user"></i>Log out</a>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a>
+          <div class="right_tools">
+          <i class="fas fa-clock" style="font-size:36px;color:white;"></i>
+          <i class="fas fa-clock" style="font-size:36px;color:white;"></i>
+          </div>
+        </div>
+        
+        <div class="div2">
+            <h1>FloraShop</h1>  
+        </div>
+        <button class='btn btn-default' id='shop-btn'><a href='#'>SHOP NOW</a></button>
+        <script src="nav.js"></script>
+      </body> 
+    
+</html>
